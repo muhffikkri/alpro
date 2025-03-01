901b3911e2a6a9c01f671ad3017d6c8b4113bc90
muhffikkri <moehammad.fikr@gmail.com>
2025-03-01 16:04:00 +0700

    Copy files to praktikum_2

---
+++ praktikum_2/1_InOut/luasKelLayang.c
@@ -0,0 +1,22 @@
+/*Nama File 	: luasKelLayang.c*/
+/*Deskripsi 	: Menghitung keliling dan luas layang-layang*/
+/*Pembuat   	: 24060124130069-Muhammad Fikri*/
+/*Tgl Pembuatan	: 28 Februari 2025 07.50 */
+
+#include <stdio.h> /*header file*/
+
+/*Program Utama*/
+int main()
+{
+    /*Kamus*/
+    int s1, s2, d1, d2, keliling, luas;
+
+    /*Algoritma*/
+    scanf("%d %d %d %d", &s1, &s2, &d1, &d2);
+    keliling = 2 * (s1 + s2);
+    luas = (d1 * d2) / 2;
+    printf("Keliling : %d\n", keliling);
+    printf("Luas : %d\n", luas);
+
+    return 0;
+}
