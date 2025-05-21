f70bde4e82baab2852e47ec44af499d635e1addd
muhffikkri <moehammad.fikr@gmail.com>
2025-05-21 10:48:53 +0700

    laprak update

--- praktikum_9/laprak.c
+++ praktikum_9/laprak.c
@@ -31,20 +31,36 @@ int sekuensial()
 
     for (i = 0; i < 101; i++)
     {
-        if (TABNILAI[i] >= 80){
+        if (TABNILAI[i] >= 80)
+        {
             TABGRADE[i] = 'A';
-	    grade[0] += 1;}
-        else if (TABNILAI[i] >= 70){
+            grade[0] += 1;
+        }
+        else if (TABNILAI[i] >= 70)
+        {
             TABGRADE[i] = 'B';
-	    grade[1] += 1;}
-        else if (TABNILAI[i] >= 55){
+            grade[1] += 1;
+        }
+        else if (TABNILAI[i] >= 55)
+        {
             TABGRADE[i] = 'C';
-	   grade[2] += 1;}
-        else if (TABNILAI[i] >= 40){
+            grade[2] += 1;
+        }
+        else if (TABNILAI[i] >= 40)
+        {
             TABGRADE[i] = 'D';
-	grade[3] += 1;}
-        else {
-            TABGRADE[i] = 'E';grade[4] += 1;}
+            grade[3] += 1;
+        }
+        else
+        {
+            TABGRADE[i] = 'E';
+            grade[4] += 1;
+        }
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        printf(grade[i]);
     }
 
     return 0;
@@ -55,25 +71,36 @@ int searching1()
     // searching with sentinel if table are sorted ascending
 
     int i;
-    int n = 101;
+    int N = 101;
     int x;
-    int sentinel;
-    int t[n];
+    int IX;
+    int t[N];
 
+    t[N + 1] = x;
+    i = 0;
+    while (t[i] != x)
+    {
+        i++;
+    }
+    if (i < N + 1)
+    {
+        IX = i;
+    }
+    else
+    {
+        IX = 0;
+    }
 
-sentinel = x
-t[n+1] = sentinel
-i = 0; while (t[i] != x) {i++} if(i <= 101) {
-
-return 0;
+    return 0;
 }
 
 int searching2()
 {
-    int i;
+    int i, j;
+    int temp;
     int NMK;
     int MKX;
-    int T[101];
+    int TABMK[101];
     int exist;
     int low;
     int high;
@@ -81,12 +108,13 @@ int searching2()
 
     for (i = 0; i < 101; i++)
     {
-        T[i] = 0;
+        TABMK[i] = 0;
     }
 
+    NMK = 0;
     for (i = 0; i < 101; i++)
     {
-        if (T[i] != 0)
+        if (TABMK[i] != 0)
             NMK++;
     }
 
@@ -94,19 +122,19 @@ int searching2()
 
     // check if MKX is in T using binary search
     low = 0;
-    high = NMK - 1;
+    high = NMK;
     mid;
     exist = 0;
     // binary search
-    while (low <= high)
+    while (low <= high && exist == 0)
     {
         mid = (low + high) / 2;
-        if (T[mid] == MKX && exist == 0 && T[mid] != 0)
+        if (TABMK[mid] == MKX && TABMK[mid] != 0)
         {
             printf("MKX %d ada di T[%d]\n", MKX, mid);
             exist = 1;
         }
-        else if (T[mid] < MKX)
+        else if (TABMK[mid] < MKX)
         {
             low = mid + 1;
         }
@@ -120,19 +148,19 @@ int searching2()
     // Table must be sorted ascending
     if (exist == 0)
     {
-        T[NMK] = MKX;
+        TABMK[NMK] = MKX;
         NMK++;
 
         // sort T in ascending order
         for (i = 0; i < NMK - 1; i++)
         {
-            for (int j = i + 1; j < NMK; j++)
+            for (j = i + 1; j < NMK; j++)
             {
-                if (T[i] > T[j])
+                if (TABMK[i] > TABMK[j])
                 {
-                    int temp = T[i];
-                    T[i] = T[j];
-                    T[j] = temp;
+                    temp = TABMK[i];
+                    TABMK[i] = TABMK[j];
+                    TABMK[j] = temp;
                 }
             }
         }
