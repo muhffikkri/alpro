b163cdb41042e0bd45e5b5425011469004dfca13
Nexvaulus <moehammad.fikr@gmail.com>
2026-02-20 10:04:00 +0700

    Remove multiple C source files related to various algorithms and functionalities, including perfect number checks, prime number checks, factor calculations, summation of series, and sorting algorithms. This commit deletes the following files: - BilSempN.c - CekBilPrima.c - CekBilSemp.c - FaktorBil.c - JumDeret.c - cekBilAll.c - FrekNilTabel.c - JumBarKolMat.c - JumFrekNilTabel.c - NilMax2Tabel.c - SimetriTabel.c - dekripsiCaesar.c - tukar.c - sequential_search.c - binary_search.c - counting_sort.c - laprak.c - selection_sort_ascending.c - selection_sort_descending.c - tugas.c

--- praktikum_10/kalkulator.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "kalkulator.h"
-
-void tambah(int *a, int b)
-{
-    /* Proses penjumlahan */
-    *a += b;
-}
--- praktikum_10/kalkulator.h
+++
@@ -1,5 +0,0 @@
-#ifndef KALKULATOR_H
-#define KALKULATOR_H
-
-void tambah(int *a, int b);
-#endif
